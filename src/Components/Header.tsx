ed364bab5c18a72a2d9303ff201ef18fcc8c856a
Aydot <ayomidotzee@gmail.com>
2023-09-08 03:24:26 -0700

    Changed resume on portfolio

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -7,7 +7,7 @@ export default function Header() {
         <div className='text-neutral-content'>
           <h1 className='mb-5 text-5xl font-bold'>Hi there</h1>
           <p className='mb-5 font-medium text-lg'>{`My name is Ayomide Ibiteye (Aydot), a frontend engineer and welcome to my corner of the internet! I'm a web developer with a passion for crafting clean and efficient code, and a love for all things tech. When I'm not busy building websites and applications, you can find me tinkering with new programming languages or daydreaming about my next big project. Thanks for stopping by!`}</p>
-          <a href={`Ayomide's_Resume.docx.pdf`} download className='btn btn-primary'>CV/Resume</a>
+          <a href={`Ayomide's CV.pdf`} download className='btn btn-primary'>CV/Resume</a>
         </div>
     </div>
   )
